4908c3bff0336d0792f354dd6206aafad79189dc
Licoorz87 <82511992+Licoorz87@users.noreply.github.com>
2021-07-23 18:35:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ from creationPopulation import createPopulation
 from creationVirus import creationName, creationProperties
 from random import random, randint
 from os import system
-from time import sleep
+import matplotlib.pyplot as plt
+
+# Print Pretty
+def clear():
+	# Pycharm / Pydroid
+	try:
+		system('clear')
+	except:
+		pass
+
+	# CMD / VSCode
+	try:
+		system('cls')
+	except:
+		pass
+
 
 # Create virus and population
 population = createPopulation(50000)
@@ -20,18 +35,23 @@ infected = 1
 immunized = 0
 dead = 0
 
+infectedDays = list()
+immunizedDays = list()
+deadDays = list()
 
 # main
 day = 0
+infectedPerMutation = 0
 while infected != 0:
 	# Per day
 	infectedToday = 0
 	immunizedToday = 0
 	deadToday = 0
 	
+	#son = [i, False, risk, 0, []]
 	for number, person in enumerate(population):
 		if person[1] == True:
-			lethalityPerson = lethality + person[2] / (maxActiveTime - incubationTime)
+			lethalityPerson = lethality + (1 * person[2]) / (maxActiveTime - incubationTime)
 			if person[3] > maxActiveTime:
 				population[number][1] = 'Immunized'
 				infected -= 1
@@ -50,7 +70,7 @@ while infected != 0:
 			population[number][3] += 1
 			
 			if incubationTime <= person[3] <= maxActiveTime:
-				contacts = randint(3, 7)
+				contacts = randint(3, 5)
 				
 				for contact in range(contacts):
 					indexPerson = randint(0, len(population)-1)
@@ -60,11 +80,11 @@ while infected != 0:
 						randomNumber = random() * 100
 						
 						if randomNumber <= transmissionTax:
-							population[indexPerson][1] = True
+							population[indexPerson][1] = person[1]
 							infected += 1
 							infectedToday += 1
 	
-	system('clear')
+	clear()
 	print(f'day {day+1}')
 	print('\nToday:')
 	print(f'infected = {infectedToday}')
@@ -74,16 +94,32 @@ while infected != 0:
 	print(f'infected = {infected}')
 	print(f'immunized = {immunized}')
 	print(f'dead = {dead}')
-	#input()
-	sleep(0.1)
 	day += 1
+	infectedPerMutation += infectedToday
+
+#	if infectedPerMutation > 1000:
+#	infectedPerMutation -= 1000
+#
+#		randomNumber = randint(1, 10000)
+#
+#		if randomNumber < mutationRate:
+#			for num, person in enumerate(population):
+#				if person[1] == 'Immunized':
+#					population[num][1] = False
+#
+#				immunized = 0
+
+
+	infectedDays.append(infectedToday)
+	immunizedDays.append(immunizedToday)
+	deadDays.append(deadToday)
 
 
 # Final Statistics
-system('clear')
+clear()
 print(nameVirus)
-listOfProperties = [transmissionTax, lethality * (maxActiveTime - incubationTime), incubationTime, maxActiveTime, mutationRate]
-nameOfProperties = ['transmission Tax', 'lethality', 'incubation Time', 'maxActiveTime', 'mutationRate (%)']
+listOfProperties = [transmissionTax, (dead * 100) / (immunized - dead), incubationTime, maxActiveTime, mutationRate]
+nameOfProperties = ['transmission Tax (%)', 'lethality (%)', 'incubation Time', 'maxActiveTime', 'mutationRate (%)']
 
 for number, property in enumerate(listOfProperties):
 	print(f'{nameOfProperties[number]} = {property}')
@@ -93,3 +129,16 @@ print('\nSince always:')
 print(f'Infected = {infected}')
 print(f'immunized = {immunized}')
 print(f'dead = {dead}')
+
+graphics = input('Show Graphics? [Y/N]\n').upper()[0]
+
+if graphics == 'Y':
+	datas = [infectedDays, immunizedDays, deadDays]
+	subtitlesData = ['Infected Per Day', 'Immunized Per Day', 'Dead Per Day']
+
+	for num, data in enumerate(datas):
+		fig, ax = plt.subplots()
+		ax.plot([i for i in range(1, day+1)], data)
+		ax.set(xlabel='Days', title='Counting of ' + subtitlesData[num])
+		ax.grid(True)
+		plt.show()
